1a246f30a1ab61e62aab5e482b7fcca20ed8a8a1
Raymundo <rverduzcoa@gmail.com>
2025-12-11 15:49:15 -0700

    quitando un video de carrusel homePage

--- js/videos.js
+++ js/videos.js
@@ -3,7 +3,6 @@
 const videosHome = [
     // Rutas de tus archivos MP4 descargados, ubicados en /video/reels/
     { id: 1, src: 'media/videos/Gaby1.mp4', titulo: 'gabyModelo' },
-    { id: 2, src: 'media/videos/nalgaManita.mp4', titulo: 'anilloPulseraModelo' },
     { id: 3, src: 'media/videos/cadenaDoradaModelada.mp4', titulo: 'cadenaModelo'},
     { id: 4, src: 'media/videos/Gaby2.mp4', titulo: 'gabyModelo'}
 ];
