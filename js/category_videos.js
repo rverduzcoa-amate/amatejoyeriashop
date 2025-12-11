88770cc2d08a5946083dd7839a0d54861444586f
Raymundo <rverduzcoa@gmail.com>
2025-12-11 15:58:49 -0700

    agregando posters para los videos

--- js/category_videos.js
+++ js/category_videos.js
@@ -6,14 +6,16 @@ const categoryVideoBanners = [
         categoria: 'pulseras', // Nombre de la categoría a cargar
         src: 'media/videos/pulseraDoradaVariados.mp4', // Video de la categoría Pulseras
         titulo: 'Pulseras Exclusivas',
-        link: '#home?categoria=pulseras' // Enlace al que irá el clic
+        link: '#home?categoria=pulseras', // Enlace al que irá el clic
+        posters: 'media/img/posters/pulseraDoradaVariados.png' 
     },
     {
         id: 'C2',
         categoria: 'aretes', // Nombre de la categoría a cargar
         src: 'media/videos/aretesArracadasCorazon.mp4',
         titulo: 'Aretes Lindos',
-        link: '#home?categoria=aretes' 
+        link: '#home?categoria=aretes',
+        posters: 'media/img/posters/aretesArracadasCorazon.png' 
     },
     // Añade más videos según tus categorías
     {
@@ -21,6 +23,7 @@ const categoryVideoBanners = [
         categoria: 'cadenas',
         src: 'media/videos/cadenaDoradaDijeRedondoNegro.mp4',
         titulo: 'Collares Elegantes',
-        link: '#home?categoria=cadenas'
+        link: '#home?categoria=cadenas',
+        posters: 'media/img/posters/cadenaDoradaDijeRedondoNegro.png'
     }
 ];
--- js/videos.js
+++ js/videos.js
@@ -2,7 +2,7 @@
 
 const videosHome = [
     // Rutas de tus archivos MP4 descargados, ubicados en /video/reels/
-    { id: 1, src: 'media/videos/Gaby1.mp4', titulo: 'gabyModelo' },
-    { id: 3, src: 'media/videos/cadenaDoradaModelada.mp4', titulo: 'cadenaModelo'},
-    { id: 4, src: 'media/videos/Gaby2.mp4', titulo: 'gabyModelo'}
+    { id: 1, src: 'media/videos/Gaby1.mp4', titulo: 'gabyModelo', posters: "media/img/posters/Gaby1.png" },
+    { id: 3, src: 'media/videos/cadenaDoradaModelada.mp4', titulo: 'cadenaModelo', posters: "media/img/posters/cadenaDoradaModelada.png"},
+    { id: 4, src: 'media/videos/Gaby2.mp4', titulo: 'gabyModelo', posters: "media/img/posters/Gaby2.png"}
 ];
